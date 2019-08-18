5df4eab6bad15faa3bcfe297633e4d6d1c1e374e
jiniC <jinic323@gmail.com>
2019-08-18 20:51:22 +0900

    refactoring 2 - UserProfile component commonization (by computed)

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,45 @@
+<template>
+    <!-- 질문 상세 정보 -->
+    <div class="user-container">
+        <div>
+            <i class="fas fa-user"></i>
+        </div>
+        <div class="user-description">
+            <div>{{ userInfo.id }}</div>
+            <!-- <router-link :to="`/user/${userInfo.id}`">
+                {{ userInfo.id }}
+            </router-link> -->
+            <div class="time">
+                {{ userInfo.created }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    // 첫 번째 방법: UserProfile에서 state로 데이터에 접근
+    computed: {
+        userInfo() {
+            return this.$store.state.user;
+        }
+    },
+}
+</script>
+
+<style scoped>
+.user-container {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+}
+.fa-user {
+    font-size: 2.5rem;
+}
+.user-description {
+    padding-left: 8px;
+}
+.time {
+    font-size: 0.7rem;
+}
+</style>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,28 +1,19 @@
 <template>
-    <!-- 태그안에는 정돈된 형태로 노출 -->
     <div>
-        <p>name: {{ userInfo.id }}</p>
-        <p>karma: {{ userInfo.karma }}</p>
-        <p>created: {{ userInfo.created_time }}</p>
+        <user-profile></user-profile>
     </div>
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile';
+
 export default {
-    computed: {
-        userInfo() {
-            return this.$store.state.user;
-        }
+    components: {
+        UserProfile,
     },
     created() {
         const userName = this.$route.params.id;
-        // dispatch 두번째인자에선 하나만 넘길 수 있음
         this.$store.dispatch('FETCH_USER', userName);
-
-        // 동적라우팅을 이용한 ItemView페이지 만들기 실습 과정
-        // 1. ItemView 생성
-        // 2. 라우터에 ItemView로 갈 수 있는 라우터 정보 등록
-        // 3. 해당 페이지 컴포넌트로 이동했을 때 받아온 params(id)를 이용해서 페이지에 데이터 표시
     },
 }
 </script>
