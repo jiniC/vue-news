bd88d3a612252e44e38f20325300ff0ab4d8f9cd
jiniC <jinic323@gmail.com>
2019-08-18 11:51:51 +0900

    add router transition

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <div id="app">
     <!-- url이 news면 <NewsView></NewsView>, ask면 ~, jobs면 ~ -->
     <!-- 템플릿명 작성시에는 카멜케이스말고 케밥케이스로 작성하기 권장 -->
-    <tool-bar></tool-bar> 
-    <router-view></router-view>
+    <tool-bar></tool-bar>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -21,5 +23,13 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+/* https://router.vuejs.org/guide/advanced/transitions.html#per-route-transition */
+/* https://vuejs.org/v2/guide/transitions.html */
+.page-enter-active, .page-leave-active {
+  transition: opacity .5s;
+}
+.page-enter, .page-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
