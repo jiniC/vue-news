3f7e88476e46c95ebef091ad8cf340c0846e4158
jiniC <jinic323@gmail.com>
2019-08-19 22:58:50 +0900

    refactoring 3 - apply mixin

---
+++ src/views/AskView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <list-item></list-item>
+    </div>
+</template>
+
+<script>
+import ListItem from '../components/ListItem';
+import bus from '../utils/bus.js';
+import ListMixin from '../mixins/ListMixin.js'
+
+export default {
+    components: {
+        ListItem,
+    },
+    mixins: [ListMixin],
+    // created() {
+    //     bus.$emit('start:spinner');
+    //     setTimeout(()=> {
+    //         this.$store.dispatch('FETCH_ASK')
+    //             .then(() => {
+    //                 console.log('fetched');
+    //                 bus.$emit('end:spinner');
+    //             })
+    //             .catch((err) => {
+    //                 console.log(err);
+    //             });
+    //     }, 1000);
+    // }
+}
+</script>
--- src/views/CreateListView.js
+++ src/views/CreateListView.js
@@ -4,7 +4,7 @@ import bus from '../utils/bus'
 // HOC: createListView (NewsView, JobsView, AskView)
 export default function createListView(name) {
     return {
-        // 재사용할 인스턴스(컴포넌트) 옵션들이 들어갈 자리
+        // 재사용할 인스턴스(컴포넌트) 옵션 & 로직들이 들어갈 자리
         name: `HOC component:${name}`,
         created() {
             bus.$emit('start:spinner');
