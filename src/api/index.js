499164460db27e078da1e71060b0c580fc976621
jiniC <jinic323@gmail.com>
2019-08-25 18:21:14 +0900

    refactoring 5 - asynchronous processing with async & await

--- src/api/index.js
+++ src/api/index.js
@@ -15,14 +15,24 @@ function fetchJobsList() {
 function fetchAskList() {
     return axios.get(`${config.baseUrl}ask/1.json`);
 }
-function fetchList(pageName) {
-	return axios.get(`${config.baseUrl}${pageName}/1.json`);
+async function fetchList(pageName) {
+    try {
+        return await axios.get(`${config.baseUrl}${pageName}/1.json`);
+    } catch (error) {
+        console.log(error);
+    }
 }
 function fetchUserInfo(username) {
     return axios.get(`${config.baseUrl}user/${username}.json`);
 }
-function fetchItemInfo(itemid) {
-    return axios.get(`${config.baseUrl}item/${itemid}.json`);
+async function fetchItemInfo(itemid) {
+    try {
+        // return await axios.get(`${config.baseUrl}item/${itemid}.json`);
+        const response = await axios.get(`${config.baseUrl}item/${itemid}.json`);
+        return response;;
+    } catch (error) {
+        console.log(error);
+    }
 }
 export {
     fetchNewsList,
--- src/store/actions.js
+++ src/store/actions.js
@@ -39,36 +39,61 @@ export default {
 	// 			console.log(err);
 	// 		});
 	// },
-	FETCH_USER({ commit }, name) {
-		return fetchUserInfo(name)
-			.then(({ data }) => {
-				commit('SET_USER', data);
-			})
-			.catch(err => {
-				console.log(err);
-			});
+
+
+	// promise 비동기:
+	// FETCH_USER({ commit }, name) {
+	// 	return fetchUserInfo(name)
+	// 		.then(({ data }) => {
+	// 			commit('SET_USER', data);
+	// 		})
+	// 		.catch(err => {
+	// 			console.log(err);
+	// 		});
+	// },
+	// async 비동기:
+	async FETCH_USER({ commit }, name) {
+		try {
+			const response = await fetchUserInfo(name);
+			commit('SET_USER', response.data);
+			return response; // 결과값을 반환해줘야함 (promise가 return됨)
+		} catch (error) {
+			console.log(error);
+		}
 	},
-	FETCH_ITEM({ commit }, item) {
-		return fetchItemInfo(item)
-			.then(({ data }) => {
-				commit('SET_ITEM', data);
-			})
-			.catch(err => {
-				console.log(err);
-			});
+
+	// FETCH_ITEM({ commit }, item) {
+	// 	return fetchItemInfo(item)
+	// 		.then(({ data }) => {
+	// 			commit('SET_ITEM', data);
+	// 		})
+	// 		.catch(err => {
+	// 			console.log(err);
+	// 		});
+	// },
+	async FETCH_ITEM({ commit }, item) {
+		// api단에서 try, catch 처리
+		const response = await fetchItemInfo(item);
+		commit('SET_ITEM', response.data);
+		return response;
 	},
 
 	// #2
-	FETCH_LIST({ commit }, pageName) {
-		// #3
-		return fetchList(pageName)
-			.then(res => {
-				// #4
-				commit('SET_LIST', res.data);
-				return res;
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	},
+	// FETCH_LIST({ commit }, pageName) {
+	// 	// #3
+	// 	return fetchList(pageName)
+	// 		.then(res => {
+	// 			// #4
+	// 			commit('SET_LIST', res.data);
+	// 			return res;
+	// 		})
+	// 		.catch(err => {
+	// 			console.log(err);
+	// 		});
+	// },
+	async FETCH_LIST({commit}, pageName) {
+		const response = await fetchList(pageName);
+		commit('SET_LIST', response.data);
+		return response;
+	}
 };
